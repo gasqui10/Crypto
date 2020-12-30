f41463e5182e232f7fab404b35fe7342de835f6f
gasqui10 <gastongasquet@gmail.com>
2020-12-30 12:02:54 -0300

    Version 1.5

---
+++ Crypto/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  Crypto
+//
+//  Created by Gaston Gasquet on 13/11/2020.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
---
+++ Crypto/Model/CoinManager.swift
@@ -0,0 +1,70 @@
+//
+//  CoinManager.swift
+//  Crypto
+//
+//  Created by Gaston Gasquet on 12/11/2020.
+//
+import Foundation
+
+protocol CoinManagerDelegate {
+    
+    //Create the method stubs wihtout implementation in the protocol.
+    //It's usually a good idea to also pass along a reference to the current class.
+    //e.g. func didUpdatePrice(_ coinManager: CoinManager, price: String, currency: String)`
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
+struct CoinManager {
+    
+    //Create an optional delegate that will have to implement the delegate methods.
+    //Which we can notify when we have updated the price.
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "DD225995-D623-410C-ADAE-3BE4C5049253"
+
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        //Optional: round the price down to 2 decimal places.
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        
+                        //Call the delegate method in the delegate (ViewController) and
+                        //pass along the necessary data.
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
